854282f65fd2c0dbb9b301b6a29cbcbf5dc6198d
moli <molix2@outlook.com>
2020-11-04 16:38:18 +0800

    add: 钩子优先级

--- app/app.go
+++ app/app.go
@@ -1,25 +1,36 @@
 package app
 
 import (
+	"sort"
+
 	"github.com/pkg/errors"
 )
 
 var app *App
 
 type Hook interface {
-	Label() string
-	Process(sess *PushSession) error
+	Label() string                   // 钩子名称
+	Priority() int                   // 钩子优先级,数字越小越先执行
+	Process(sess *PushSession) error // 执行钩子
 }
 
 type App struct {
-	hooks map[string]Hook
+	hooks []Hook
 }
 
-func (a *App) RegisterHook(h Hook) {
-	a.hooks[h.Label()] = h
+func (a *App) RegisterHook(hooks ...Hook) {
+	a.hooks = append(a.hooks, hooks...)
+}
+
+func (a *App) sortHooksByPriority() {
+	sort.Slice(a.hooks, func(i, j int) bool {
+		return a.hooks[i].Priority() < a.hooks[j].Priority()
+	})
 }
 
 func (a *App) Run(sess *PushSession) error {
+	a.sortHooksByPriority()
+
 	for _, hook := range a.hooks {
 		if err := hook.Process(sess); err != nil {
 			return err
--- app/hook_event.go
+++ app/hook_event.go
@@ -1,5 +1,9 @@
 package app
 
+import (
+	"github.com/chanxuehong/util/math"
+)
+
 var _ Hook = (*HookEvent)(nil)
 
 // 创建推送事件
@@ -8,6 +12,10 @@ type HookEvent struct {
 
 func (h HookEvent) Label() string {
 	return "event"
+}
+
+func (h HookEvent) Priority() int {
+	return math.MaxInt
 }
 
 func (h HookEvent) Process(sess *PushSession) error {
